b3c4c7a4f1cad1a691acacec3df098f157329568
Maryi Liliana Bustos <maryie098@hotmail.com>
2021-08-11 12:37:11 -0500

    Funcion mostrar para mostrar portadas.

--- src/data.js
+++ src/data.js
@@ -1,23 +1,24 @@
 // estas funciones son de ejemplo
 
-export {ordenarData}
-//let arrAux=[]
-    //let dataOrdenada = data.rt_score
-    function ordenarData(data){
+export { ordenarData }
+// let arrAux=[]
+//let dataOrdenada = data.rt_score
+function ordenarData(data) {
 
   let arrAux = data.sort((a, b) => {
-      if (parseInt(a.rt_score) < parseInt(b.rt_score) ){
-        return -1;
-      }
-      if (parseInt(a.rt_score) > parseInt(b.rt_score) ){
-        return 1;
-      }
-      return 0;
-    });
+    // arrAux = data
+    if (parseInt(a.rt_score) < parseInt(b.rt_score)) {
+      return -1;
+    }
+    if (parseInt(a.rt_score) > parseInt(b.rt_score)) {
+      return 1;
+    }
+    return 0;
+  });
+  return arrAux;
+};
 
-    //return arrAux;
-    console.log(arrAux);
-}
-//export const anotherExample = () => {
-//  return 'OMG';
-//};
+export const ordenarDataAz = function (data) {
+  return data.sort((a, b) => a.title > b.title);
+};
+
