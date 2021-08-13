c05e3bf6e77e279bd1f3410e34b1abf0b496a6d1
Luisa Saenz <lsaenzroa@gmail.com>
2021-08-13 12:58:29 -0500

    Se suben cambios y fincion de filtrar

--- src/data.js
+++ src/data.js
@@ -1,9 +1,32 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+export {ordenarData}
+//let arrAux=[]
+    //let dataOrdenada = data.rt_score
+  function ordenarData(data){
+  let arrAux = data.sort((a, b) => {
+      if (parseInt(a.rt_score) < parseInt(b.rt_score) ){
+        return 1;
+      }
+      if (parseInt(a.rt_score) > parseInt(b.rt_score) ){
+        return -1;
+      }
+      return 0;
+    });
+    //console.log(arrAux);
+    return arrAux;
+    
+}
+export {filtroLoMasPro}
+function filtroLoMasPro(mayorPuntaje){
+  let resultado = mayorPuntaje.filter(item=>{
+    return item.rt_score >=70;
+    });
+    return resultado;
+}
+/*export const ordenAcendente = function(data){
+  return data.sort((a,b)=>a.)
+}*/
+//export const anotherExample = () => {
+//  return 'OMG';
+//};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,37 @@
-import { example } from './data.js';
+import { ordenarData,filtroLoMasPro } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+//console.log(example(data./films));
 
-console.log(example, data);
+
+const rankingPeliculas = document.getElementById("item");
+rankingPeliculas.addEventListener("click", function(){
+const desple = document.getElementById("desple");
+desple.classList.toggle("mostrar")
+});
+
+let listaPeliculas = ordenarData(data.films);
+//console.log(listaPeliculas[0].poster);
+mostrarPoster(listaPeliculas);
+
+function mostrarPoster(arrpeliculas){
+   arrpeliculas.forEach((pelicula)=>{
+    let poster = document.createElement("IMG");
+    poster.setAttribute("src", pelicula.poster);
+    document.querySelector(".segundapagina").appendChild(poster);
+  
+});
+
+const bienvenida = document.querySelector(".primerapagina");
+const loMasPro = document.querySelector(".altopuntaje");
+const contenedorSegunaPagina = document.querySelector(".segundapagina");
+loMasPro.addEventListener("click",()=>{
+  contenedorSegunaPagina.style.display="block";
+  bienvenida.classList.add("esconder");
+  loMasPro.classList.remove("esconder");
+  let mayorPuntaje = ordenarData(data.films);
+  let arryLoMasPro = filtroLoMasPro(mayorPuntaje);
+  mostrarPoster(arryLoMasPro);
+});
+}
